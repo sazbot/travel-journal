0687dd1c2073eb784f5e58db135d470c7c3cf344
Sarah Fielding Smith <s.fieldingsmith@gmail.com>
2022-05-02 15:09:51 +0100

    complete navbar

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="header__nav">
+      <i className="header__logo fa-solid fa-earth-americas"></i>
+      <h1 className="header__title">my travel journal.</h1>
+    </div>
+  );
+}
